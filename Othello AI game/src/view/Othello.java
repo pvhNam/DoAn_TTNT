015e929775700c7c58b4e60ce6fc26f1d1b81ba0
nam <nam@nam-ASUS-TUF-Gaming-F15-FX506LHB-FX506LHB>
2025-11-19 10:39:49 +0700

    nam

--- Othello AI game/src/view/Othello.java
+++ Othello AI game/src/view/Othello.java
@@ -6,42 +6,29 @@ import java.awt.event.*;
 
 public class Othello extends JFrame {
 	private JButton[][] board = new JButton[8][8];
-	private int[][] boardState = new int[8][8];
 	private int currentPlayer = 1;
-	
-	private JLabel lblBlackScore;
-    private JLabel lblWhiteScore;
+
 	public Othello() {
 		setTitle("Othello");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 600);
-		setResizable(false);
-		
-		JPanel scorePanel = new JPanel();
-        scorePanel.setLayout(new FlowLayout());
-		lblBlackScore = new JLabel("⚫: 2");
-        lblWhiteScore = new JLabel("⚪: 2");
-        scorePanel.add(lblBlackScore);
-        scorePanel.add(lblWhiteScore);
-        add(scorePanel, BorderLayout.NORTH);
-		
-        JPanel boardPanel = new JPanel(new GridLayout(8, 8));
+		setLayout(new GridLayout(8, 8));
+
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				board[i][j] = new JButton();
-				board[i][j].setBackground(Color.CYAN);
+				board[i][j].setBackground(Color.BLUE);
 				board[i][j].addActionListener(new CellClick(i, j));
-				boardPanel.add(board[i][j]);
+				add(board[i][j]);
 			}
 		}
-		add(boardPanel, BorderLayout.CENTER);
-		
-		  board[3][3].setBackground(Color.WHITE);
-	      board[3][4].setBackground(Color.BLACK);
-	      board[4][3].setBackground(Color.BLACK);
-	      board[4][4].setBackground(Color.WHITE);
+		// vị trí cố dịnh khi bắt đầu
+		board[3][3].setBackground(Color.WHITE);
+		board[3][4].setBackground(Color.BLACK);
+		board[4][3].setBackground(Color.BLACK);
+		board[4][4].setBackground(Color.WHITE);
 
-        setVisible(true);
+		setVisible(true);
 
 	}
 
@@ -54,6 +41,7 @@ public class Othello extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
+			System.out.println("Clicked: " + x + "," + y);
 		}
 		
 	}
